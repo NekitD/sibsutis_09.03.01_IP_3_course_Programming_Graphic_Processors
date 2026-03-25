6fd4e686ae8533a89825770c32476126b1cf39ea
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2026-03-25 16:38:48 +0700

    rgr utils fix

--- РГР/src/utils.h
+++ РГР/src/utils.h
@@ -13,18 +13,15 @@ double get_time() {
     return tv.tv_sec + tv.tv_usec * 1e-6;
 }
 
-// Функция активации сигмоида
 __device__ __host__ inline float sigmoid(float z) {
     return 1.0f / (1.0f + expf(-z));
 }
 
-// Производная сигмоида
 __device__ __host__ inline float sigmoid_prime(float z) {
     float s = sigmoid(z);
     return s * (1.0f - s);
 }
 
-// Функция стоимости (MSE)
 __device__ __host__ inline float cost_derivative(float output, float target) {
     return output - target;
 }
